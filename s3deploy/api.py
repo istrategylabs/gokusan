469d869811eb3dd883793faf63b67ef4254f20c5
Julian Gindi <juliangindi@gmail.com>
2015-12-07 15:55:56 -0500

    error handling on configuration file not found

--- s3deploy/api.py
+++ s3deploy/api.py
@@ -9,9 +9,12 @@ def read_config():
 def read_config_file():
     current_dir = os.getcwd()
     config_path = current_dir + '/s3Deploy.cfg'
-    with open(config_path, 'r') as f:
-        contents = yaml.load(f)
-        print(contents)
+    try:
+        with open(config_path, 'r') as f:
+            contents = yaml.load(f)
+            print(contents)
+    except OSError:
+        print("s3Deploy configuration file not found.")
 
 if __name__ == "__main__":
     read_config_file()
